c108749a0212fbc80e72796e77a55faf478966b0
alpcihan <alpcihan.ac@gmail.com>
2023-04-28 20:02:35 +0200

    add auto shader compile

--- resources/shaders/compile.bat
+++ resources/shaders/compile.bat
@@ -1,3 +1,6 @@
+if not exist bin (
+    mkdir bin
+)
 C:/VulkanSDK/1.3.236.0/Bin/glslc.exe default.vert -o bin/default_vert.spv
 C:/VulkanSDK/1.3.236.0/Bin/glslc.exe default.frag -o bin/default_frag.spv
 pause
